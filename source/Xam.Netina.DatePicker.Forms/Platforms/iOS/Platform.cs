4d70941764396aacd97c0377c2f18338119a7e13
Amir.H Khademi <avvampier@gmail.com>
2023-02-16 19:23:43 +0330

    complete ios platform , add screenshots and landing

---
+++ source/Xam.Netina.DatePicker.Forms/Platforms/iOS/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rg.Plugins.Popup;
+
+namespace Xam.Netina.DatePicker.Forms.Platforms.iOS
+{
+    public static class Platform
+    {
+        public static void Init()
+        {
+            Popup.Init();
+        }
+    }
+}
